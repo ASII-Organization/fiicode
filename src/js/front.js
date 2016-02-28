93cef3f9fe4e5a94afa6415caabed2c53332e8b3
StoianCatalin <shaggy.cata@gmail.com>
2016-02-28 16:05:40 +0200

    Am adaugat fisierul javascript pentru frontend

--- src/js/front.js
+++ src/js/front.js
@@ -2,3 +2,13 @@
  * Created by Catalin on 2/28/2016.
  */
 $('.mobile .button.dropdown').dropdown();
+$(document).on('mouseover', '#homecover .ui.button.web', function() {
+    $('.backgroundhome.web').addClass('show');
+}).on('mouseleave', '#homecover .ui.button', function() {
+    $('.backgroundhome.web').removeClass('show');
+});
+$(document).on('mouseover', '#homecover .ui.button.algo', function() {
+    $('.backgroundhome.algo').addClass('show');
+}).on('mouseleave', '#homecover .ui.button', function() {
+    $('.backgroundhome.algo').removeClass('show');
+});
